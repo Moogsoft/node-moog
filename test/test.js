e0a5db8e96fe23fef6e4b1d0d15c85716d82c412
stephen <stephen@moogsoft.com>
2014-11-03 12:23:32 +0000

    Update to use that not self, and add use strict, this required some changes to debug as it was using callee.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
  */
 
 var nm = require('node-moog');
+var util = require('util');
 var options = {
     'url': 'https://innomoog:8881',
     'auth_token': 'my_secret',
@@ -14,12 +15,11 @@ var moogRest = new nm.MoogREST(options);
 
 var debug = function () {
     if (!process.env.DEBUG) return;
-
-    var now = new Date(),
-        header = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + " [Test.js]" + arguments.callee.caller.name + " -> ",
-        args = Array.prototype.slice.call(arguments);
-    args.splice(0, 0, header);
-    console.log.apply(console, args);
+    var stack = new Error().stack;
+    var args = Array.prototype.slice.call(arguments);
+    var lines = stack.split('\n');
+    var callee = lines[2].match(/at .* /i); //used because of use strict
+    util.debug('[test.js] '+callee+' -> '+args);
 };
 
 moogEvent.description = 'My new description';
@@ -27,12 +27,12 @@ debug('Event generated ' + JSON.stringify(moogEvent));
 
 moogRest.sendEvent(moogEvent, function (res, rtn) {
     if (rtn == 200) {
-        console.log('moogRest message sent, return code: ' + rtn);
-        console.log('moogRest result: ' + res.message);
+        util.log('moogRest message sent, return code: ' + rtn);
+        util.log('moogRest result: ' + res.message);
         process.exit(0);
     } else {
-        console.log('moogRest - ' + rtn);
-        console.log('moogRest - ' + res);
+        util.error('moogRest - ' + rtn);
+        util.error('moogRest - ' + res);
         process.exit(1);
     }
 });
